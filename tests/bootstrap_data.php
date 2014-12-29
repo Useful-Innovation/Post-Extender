f7a5e8451fc0a70d5402ed5a8136b43112b6d07f
Emil Lunnergård <emil.lunnergard@gmail.com>
2014-12-29 12:47:17 +0100

    Modularize bootstrap data

--- tests/bootstrap_data.php
+++ tests/bootstrap_data.php
@@ -3,6 +3,7 @@
 class DATA
 {
   public static $POST_1;
+  public static $POSTS;
 }
 
 DATA::$POST_1 = [
@@ -14,7 +15,9 @@ DATA::$POST_1 = [
     'field_duplicated' => "0",
     'field_type'       => "image_media",
     'group_name'       => "info",
-    'group_duplicated' => "0"
+    'group_duplicated' => "0",
+    'options'          => serialize([
+    ])
   ],
   (object)[
     'ID'               => "2",
@@ -24,7 +27,9 @@ DATA::$POST_1 = [
     'field_duplicated' => "1",
     'field_type'       => "textbox",
     'group_name'       => "info",
-    'group_duplicated' => "0"
+    'group_duplicated' => "0",
+    'options'          => serialize([
+    ])
   ],
   (object)[
     'ID'               => "2",
@@ -34,7 +39,9 @@ DATA::$POST_1 = [
     'field_duplicated' => "1",
     'field_type'       => "textbox",
     'group_name'       => "info",
-    'group_duplicated' => "0"
+    'group_duplicated' => "0",
+    'options'          => serialize([
+    ])
   ],
   (object)[
     'ID'               => "2",
@@ -44,7 +51,9 @@ DATA::$POST_1 = [
     'field_duplicated' => "0",
     'field_type'       => "file",
     'group_name'       => "test",
-    'group_duplicated' => "1"
+    'group_duplicated' => "1",
+    'options'          => serialize([
+    ])
   ],
   (object)[
     'ID'               => "2",
@@ -54,7 +63,9 @@ DATA::$POST_1 = [
     'field_duplicated' => "0",
     'field_type'       => "checkbox",
     'group_name'       => "test",
-    'group_duplicated' => "1"
+    'group_duplicated' => "1",
+    'options'          => serialize([
+    ])
   ],
   (object)[
     'ID'               => "2",
@@ -64,7 +75,9 @@ DATA::$POST_1 = [
     'field_duplicated' => "0",
     'field_type'       => "dropdown",
     'group_name'       => "test",
-    'group_duplicated' => "1"
+    'group_duplicated' => "1",
+    'options'          => serialize([
+    ])
   ],
   (object)[
     'ID'               => "2",
@@ -74,7 +87,9 @@ DATA::$POST_1 = [
     'field_duplicated' => "0",
     'field_type'       => "checkbox_list",
     'group_name'       => "test",
-    'group_duplicated' => "1"
+    'group_duplicated' => "1",
+    'options'          => serialize([
+    ])
   ],
   (object)[
     'ID'               => "2",
@@ -84,6 +99,240 @@ DATA::$POST_1 = [
     'field_duplicated' => "0",
     'field_type'       => "related_type",
     'group_name'       => "test",
-    'group_duplicated' => "1"
+    'group_duplicated' => "1",
+    'options'          => serialize([
+    ])
+  ]
+];
+
+
+DATA::$POSTS = [
+  (object)[
+    'ID'               => "2",
+    'meta_key'         => "info_image",
+    'meta_value'       => "4",
+    'group_count'      => "1",
+    'field_duplicated' => "0",
+    'field_type'       => "image_media",
+    'group_name'       => "info",
+    'group_duplicated' => "0",
+    'options'          => serialize([
+    ])
+  ],
+  (object)[
+    'ID'               => "2",
+    'meta_key'         => "info_title",
+    'meta_value'       => "asd",
+    'group_count'      => "1",
+    'field_duplicated' => "1",
+    'field_type'       => "textbox",
+    'group_name'       => "info",
+    'group_duplicated' => "0",
+    'options'          => serialize([
+    ])
+  ],
+  (object)[
+    'ID'               => "2",
+    'meta_key'         => "info_title",
+    'meta_value'       => "qwe",
+    'group_count'      => "1",
+    'field_duplicated' => "1",
+    'field_type'       => "textbox",
+    'group_name'       => "info",
+    'group_duplicated' => "0",
+    'options'          => serialize([
+    ])
+  ],
+  (object)[
+    'ID'               => "2",
+    'meta_key'         => "test_file",
+    'meta_value'       => "1397294301Bonnr1190009484.pdf",
+    'group_count'      => "1",
+    'field_duplicated' => "0",
+    'field_type'       => "file",
+    'group_name'       => "test",
+    'group_duplicated' => "1",
+    'options'          => serialize([
+    ])
+  ],
+  (object)[
+    'ID'               => "2",
+    'meta_key'         => "test_checker",
+    'meta_value'       => "1",
+    'group_count'      => "1",
+    'field_duplicated' => "0",
+    'field_type'       => "checkbox",
+    'group_name'       => "test",
+    'group_duplicated' => "1",
+    'options'          => serialize([
+    ])
+  ],
+  (object)[
+    'ID'               => "2",
+    'meta_key'         => "test_select",
+    'meta_value'       => 'a:1:{i:0;s:8:"Option 2";}',
+    'group_count'      => "1",
+    'field_duplicated' => "0",
+    'field_type'       => "dropdown",
+    'group_name'       => "test",
+    'group_duplicated' => "1",
+    'options'          => serialize([
+    ])
+  ],
+  (object)[
+    'ID'               => "2",
+    'meta_key'         => "test_checkboxes",
+    'meta_value'       => 'a:1:{i:0;s:8:"Option 1";}',
+    'group_count'      => "1",
+    'field_duplicated' => "0",
+    'field_type'       => "checkbox_list",
+    'group_name'       => "test",
+    'group_duplicated' => "1",
+    'options'          => serialize([
+    ])
+  ],
+  (object)[
+    'ID'               => "2",
+    'meta_key'         => "test_related",
+    'meta_value'       => "2",
+    'group_count'      => "1",
+    'field_duplicated' => "0",
+    'field_type'       => "related_type",
+    'group_name'       => "test",
+    'group_duplicated' => "1",
+    'options'          => serialize([
+    ])
+  ],
+  (object)[
+    'ID'               => "7",
+    'meta_key'         => "info_image",
+    'meta_value'       => "4",
+    'group_count'      => "1",
+    'field_duplicated' => "0",
+    'field_type'       => "image_media",
+    'group_name'       => "info",
+    'group_duplicated' => "0",
+    'options'          => serialize([
+    ])
+  ],
+  (object)[
+    'ID'               => "7",
+    'meta_key'         => "info_title",
+    'meta_value'       => "caption 1",
+    'group_count'      => "1",
+    'field_duplicated' => "1",
+    'field_type'       => "textbox",
+    'group_name'       => "info",
+    'group_duplicated' => "0",
+    'options'          => serialize([
+    ])
+  ],
+  (object)[
+    'ID'               => "7",
+    'meta_key'         => "info_title",
+    'meta_value'       => "caption 2",
+    'group_count'      => "1",
+    'field_duplicated' => "1",
+    'field_type'       => "textbox",
+    'group_name'       => "info",
+    'group_duplicated' => "0",
+    'options'          => serialize([
+    ])
+  ],
+  (object)[
+    'ID'               => "7",
+    'meta_key'         => "info_title",
+    'meta_value'       => "caption 3",
+    'group_count'      => "1",
+    'field_duplicated' => "1",
+    'field_type'       => "textbox",
+    'group_name'       => "info",
+    'group_duplicated' => "0",
+    'options'          => serialize([
+    ])
+  ],
+  (object)[
+    'ID'               => "7",
+    'meta_key'         => "test_file",
+    'meta_value'       => "1397294593Bonnr1190009484.pdf",
+    'group_count'      => "1",
+    'field_duplicated' => "0",
+    'field_type'       => "file",
+    'group_name'       => "test",
+    'group_duplicated' => "1",
+    'options'          => serialize([
+    ])
+  ],
+  (object)[
+    'ID'               => "7",
+    'meta_key'         => "test_checker",
+    'meta_value'       => "1",
+    'group_count'      => "1",
+    'field_duplicated' => "0",
+    'field_type'       => "checkbox",
+    'group_name'       => "test",
+    'group_duplicated' => "1",
+    'options'          => serialize([
+    ])
+  ],
+  (object)[
+    'ID'               => "7",
+    'meta_key'         => "test_select",
+    'meta_value'       => 'a:1:{i:0;s:8:"Option 1";}',
+    'group_count'      => "1",
+    'field_duplicated' => "0",
+    'field_type'       => "dropdown",
+    'group_name'       => "test",
+    'group_duplicated' => "1",
+    'options'          => serialize([
+    ])
+  ],
+  (object)[
+    'ID'               => "7",
+    'meta_key'         => "test_checkboxes",
+    'meta_value'       => 'a:1:{i:0;s:8:"Option 2";}',
+    'group_count'      => "1",
+    'field_duplicated' => "0",
+    'field_type'       => "checkbox_list",
+    'group_name'       => "test",
+    'group_duplicated' => "1",
+    'options'          => serialize([
+    ])
+  ],
+  (object)[
+    'ID'               => "7",
+    'meta_key'         => "test_related",
+    'meta_value'       => "2",
+    'group_count'      => "1",
+    'field_duplicated' => "0",
+    'field_type'       => "related_type",
+    'group_name'       => "test",
+    'group_duplicated' => "1",
+    'options'          => serialize([
+    ])
+  ],
+  (object)[
+    'ID'               => "7",
+    'meta_key'         => "test_content",
+    'meta_value'       => "this is content",
+    'group_count'      => "1",
+    'field_duplicated' => "0",
+    'field_type'       => "multiline",
+    'group_name'       => "test",
+    'group_duplicated' => "1",
+    'options'          => serialize([
+    ])
+  ],
+  (object)[
+    'ID'               => "7",
+    'meta_key'         => "test_content",
+    'meta_value'       => "this is content",
+    'group_count'      => "1",
+    'field_duplicated' => "0",
+    'field_type'       => "markdown_editor",
+    'group_name'       => "test",
+    'group_duplicated' => "1",
+    'options'          => serialize([
+    ])
   ]
 ];
