e8d12a150b921e8788843a79354c5795f38f723b
Emil Lunnergård <emil.lunnergard@gmail.com>
2014-12-29 12:17:32 +0100

    modularize bootstrap data

---
+++ tests/bootstrap_data.php
@@ -0,0 +1,89 @@
+<?php
+
+class DATA
+{
+  public static $POST_1;
+}
+
+DATA::$POST_1 = [
+  (object)[
+    'ID'               => "2",
+    'meta_key'         => "info_image",
+    'meta_value'       => "4",
+    'group_count'      => "1",
+    'field_duplicated' => "0",
+    'field_type'       => "image_media",
+    'group_name'       => "info",
+    'group_duplicated' => "0"
+  ],
+  (object)[
+    'ID'               => "2",
+    'meta_key'         => "info_title",
+    'meta_value'       => "asd",
+    'group_count'      => "1",
+    'field_duplicated' => "1",
+    'field_type'       => "textbox",
+    'group_name'       => "info",
+    'group_duplicated' => "0"
+  ],
+  (object)[
+    'ID'               => "2",
+    'meta_key'         => "info_title",
+    'meta_value'       => "qwe",
+    'group_count'      => "1",
+    'field_duplicated' => "1",
+    'field_type'       => "textbox",
+    'group_name'       => "info",
+    'group_duplicated' => "0"
+  ],
+  (object)[
+    'ID'               => "2",
+    'meta_key'         => "test_file",
+    'meta_value'       => "1397294301Bonnr1190009484.pdf",
+    'group_count'      => "1",
+    'field_duplicated' => "0",
+    'field_type'       => "file",
+    'group_name'       => "test",
+    'group_duplicated' => "1"
+  ],
+  (object)[
+    'ID'               => "2",
+    'meta_key'         => "test_checker",
+    'meta_value'       => "1",
+    'group_count'      => "1",
+    'field_duplicated' => "0",
+    'field_type'       => "checkbox",
+    'group_name'       => "test",
+    'group_duplicated' => "1"
+  ],
+  (object)[
+    'ID'               => "2",
+    'meta_key'         => "test_select",
+    'meta_value'       => 'a:1:{i:0;s:8:"Option 2";}',
+    'group_count'      => "1",
+    'field_duplicated' => "0",
+    'field_type'       => "dropdown",
+    'group_name'       => "test",
+    'group_duplicated' => "1"
+  ],
+  (object)[
+    'ID'               => "2",
+    'meta_key'         => "test_checkboxes",
+    'meta_value'       => 'a:1:{i:0;s:8:"Option 1";}',
+    'group_count'      => "1",
+    'field_duplicated' => "0",
+    'field_type'       => "checkbox_list",
+    'group_name'       => "test",
+    'group_duplicated' => "1"
+  ],
+  (object)[
+    'ID'               => "2",
+    'meta_key'         => "test_related",
+    'meta_value'       => "2",
+    'group_count'      => "1",
+    'field_duplicated' => "0",
+    'field_type'       => "related_type",
+    'group_name'       => "test",
+    'group_duplicated' => "1"
+  ]
+];
